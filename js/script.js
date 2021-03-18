d93b3f340a8710601ead055ef0532a23c9597fda
karin valcanover <valcanoverkarin@outlook.it>
2021-03-18 15:38:24 +0100

    nome cognome colore

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+var nomeUtente = prompt('Inserisci il tuo nome');
+console.log(nomeUtente);
+
+var cognomeUtente = prompt('Inserisci il tuo cognome');
+console.log(cognomeUtente);
+
+var coloreUtente = prompt('Qual è il tuo colore preferito?');
+console.log(coloreUtente);
